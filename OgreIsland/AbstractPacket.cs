7b780c6e786aa144ad4b7fa44adcf7d095c7f225
Benjamin Manns <benmanns@gmail.com>
2010-08-16 11:48:03 -0400

    Created AbstractPacket class.

---
+++ OgreIsland/AbstractPacket.cs
@@ -0,0 +1,14 @@
+﻿using OgreIsland.Packets;
+
+namespace OgreIsland
+{
+    public class AbstractPacket
+    {
+        protected Mode Mode { get; set; }
+        public Packet Packet { get; private set; }
+        public string Command { get { return Packet.Command; } set { Packet.Command = value; } }
+        public string[] Arguments { get { return Packet.Arguments; } set { Packet.Arguments = value; } }
+        public AbstractPacket(Packet packet) { Packet = packet; }
+        public AbstractPacket(Packet packet, Mode mode) { Packet = packet; Mode = mode; }
+    }
+}
