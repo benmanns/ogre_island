ca56b7c99e748b0f2584ee4a41fea60f45b80b1d
Benjamin Manns <benmanns@gmail.com>
2010-08-16 11:48:15 -0400

    Created PacketList class.

---
+++ OgreIsland/PacketList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace OgreIsland
+{
+    public class PacketList : List<AbstractPacket>
+    {
+        public PacketList(AbstractPacket packet) { Add(packet); }
+        public PacketList(Packet packet) { Add(new AbstractPacket(packet)); }
+        public PacketList(IEnumerable<AbstractPacket> packetList) { AddRange(packetList); }
+        public PacketList() { }
+    }
+}
