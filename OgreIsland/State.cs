5d61fd0962f77fd806086dc2267226c75e348b22
Benjamin Manns <benmanns@gmail.com>
2010-08-16 13:21:22 -0400

    Created State class.

---
+++ OgreIsland/State.cs
@@ -0,0 +1,13 @@
+﻿namespace OgreIsland
+{
+    public class State
+    {
+        public byte[] Buffer { get; set; }
+        public System.Net.Sockets.Socket Socket { get; set; }
+        public PacketList PacketList { get; set; }
+        public State() { }
+        public State(System.Net.Sockets.Socket socket, byte[] buffer) { Socket = socket; Buffer = buffer; }
+        public State(System.Net.Sockets.Socket socket, PacketList packetList) { Socket = socket; PacketList = packetList; }
+        public State(System.Net.Sockets.Socket socket) { Socket = socket; }
+    }
+}
