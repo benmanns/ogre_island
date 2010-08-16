148f184832f6e8d4c33848f2259fd0a255dee96a
Benjamin Manns <benmanns@gmail.com>
2010-08-16 11:47:20 -0400

    Created Packet class.

---
+++ OgreIsland/Packet.cs
@@ -0,0 +1,9 @@
+﻿namespace OgreIsland
+{
+    public class Packet
+    {
+        public string Command { get; set; }
+        public string[] Arguments { get; set; }
+        public Packet(string command, string[] arguments) { Command = command; Arguments = arguments; }
+    }
+}
